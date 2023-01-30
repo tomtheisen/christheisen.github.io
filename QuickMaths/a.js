6c0beb731c0f7dc1a63788ba3d4bc93effb7076d
Chris Theisen <grumdrig333@gmail.com>
2023-01-29 22:42:12 -0600

    Add files via upload

---
+++ QuickMaths/a.js
@@ -0,0 +1,191 @@
+'use strict';
+
+//group math facts
+	//2+3=5 >next equation> 5-3=2
+
+//math levels based on: https://mathfactsmatter.com/the-7-levels-of-math-fact-fluency/#
+//A min/max is the sum
+	//sum = rand min-max
+	//addend = 0-sum
+	//calculate other addend
+//S min/max is the minuend
+	//minuend = rand min-max
+	//subtrahend = rand 0-minuend
+	//calculate difference
+//M min/max is the max of the two multiplicands; other multiplicand is 0-10
+	//temp = rand min-max
+	//rand 0/1 for a or b
+	//other multiplicand = rand 0-10
+	//calculate product
+//D min/max is the max of the divisor and quotient
+	//temp = rand min-max
+	//rand 0/1 for divisor or quotient
+	//other (divisor or quotient) = rand 0-10
+	//calculate dividend
+	
+let difficulty = 0;
+let score = 0;
+let a=0,b=0,c=0,s='+';
+const eq = document.getElementById('equation');
+const ans = document.getElementById('answer');
+const lvl = document.getElementById('difficulty');
+const prog = document.getElementById('progress');
+	
+const levels = [
+	{//0: purple
+		A:{min:0, max:5}
+	},
+	{//1: red
+		A:{min:6, max:10},
+		S:{min:0, max:5}
+	},
+	{//2: orange
+		A:{min:11, max:15},
+		S:{min:6, max:10}
+	},
+	{//3: yellow
+		A:{min:16, max:20},
+		S:{min:11, max:15},
+		M:{min:0, max:2}
+	},
+	{//4: green
+		A:{min:21, max:25},
+		S:{min:16, max:20},
+		M:{min:3, max:5},
+		D:{min:0, max:2}
+	},
+	{//5: light blue
+		A:{min:26, max:30},
+		S:{min:21, max:25},
+		M:{min:6, max:9},
+		D:{min:3, max:5}
+	},
+	{//6: dark blue
+		A:{min:31, max:35},
+		S:{min:26, max:30},
+		M:{min:10, max:12},
+		D:{min:6, max:9}
+	},
+	{//7: purple
+		A:{min:36, max:40},
+		S:{min:31, max:35},
+		M:{min:13, max:15},
+		D:{min:10, max:12}
+	}
+	
+];
+
+function pickAKey(input){
+	const keys = Object.keys(input);
+	const index = randomInt(0, keys.length-1);
+	return keys[index];
+}
+function pickOne(array){
+	const index = randomInt(0, array.length)
+	const option = array[index];
+	return option;
+}
+function checkAnswer(){
+	const x = Number(ans.value);
+	if(x===c){
+		score++;
+		launchFirework(3);
+	}
+	else{
+		score--;
+	}
+	while(score > 5 && difficulty < 8){
+		score-=5;
+		difficulty++;
+		launchFirework(10);
+	}
+	while(score < -5 && difficulty > 0){
+		score+=5;
+		difficulty--;
+	}
+	prog.value = score+5;
+	lvl.textContent=difficulty;
+	generateEquation();
+	ans.value = '';
+	ans.focus();
+}
+
+ans.addEventListener('keydown', (e) => {
+	const keys = ['ArrowLeft','ArrowRight','Backspace','Delete'];
+	if(e.key.toLowerCase() === 'enter'){
+		checkAnswer();
+		return;
+	}
+	if(!'0123456789'.includes(e.key) && !keys.includes(e.key)){
+		e.preventDefault();
+		return;
+	}
+});
+
+function randomInt(min, max){
+	min = Math.ceil(min);
+	max = Math.floor(max)+1;//makes the max inclusive
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateEquation(){
+	const key = pickAKey(levels[difficulty]);
+	const min = levels[difficulty][key].min;
+	const max = levels[difficulty][key].max;
+	
+	switch(key){
+		case 'A':{
+			s = '+';
+			c = randomInt(min, max);
+			a = randomInt(min, c);
+			b = c-a;
+			break;
+		}
+		case 'S':{
+			s = '-';
+			a = randomInt(min, max);
+			b = randomInt(min, a);
+			c = a-b;
+			break;
+		}
+		case 'M':{
+			s='x';
+			const temp = randomInt(min, max);
+			const aorb = randomInt(0,1);
+			if(aorb === 0){
+				a = temp;
+				b = randomInt(0,10);
+			}
+			else{
+				a = randomInt(0,10);
+				b = temp;
+			}
+			c = a*b;
+			break;
+		}
+		case 'D':{
+			s='÷';
+			const temp = randomInt(min, max);
+			const aorb = randomInt(0,1);
+			if(aorb === 0){
+				b = Math.max(temp, 1);
+				c = randomInt(0,10);
+			}
+			else{
+				b = randomInt(1,10);
+				c = temp;
+			}
+			a = b*c;
+			break;
+		}
+		default:{
+			//shouldn't happen.
+			break;
+		}
+	}
+	
+	equation.textContent = `${a}${s}${b}`;
+}
+
+generateEquation();
+ans.focus();
---
+++ QuickMaths/f.js
@@ -0,0 +1,182 @@
+"use strict";
+
+const F = document.getElementById('fireworks');
+const A = document.getElementById('answer');
+const ctx = F.getContext("2d", {alpha: true});
+F.addEventListener('click', (e) => {A.focus();});
+
+//TODO: adjust these
+const speed = 10;
+const G = -.05;
+const twoPI = Math.PI*2;
+const fw = [];
+let W = 10;
+let H = 10;
+let AF = 0;
+function point(x, y){ this.x = x||0; this.y = y||0; }
+point.prototype.plus = function(addend){
+	const x = addend?.x || 0;
+	const y = addend?.y || 0;
+	
+	return new point(this.x+x,this.y+y);
+}
+point.prototype.equals = function(rhs){
+	const x = rhs?.x || 0;
+	const y = rhs?.y || 0;
+	
+	return this.x===x && this.y===y;
+}
+point.prototype.scale = function(mult){
+	return new point(this.x*mult, this.y*mult);
+}
+
+function manageFireworks(){
+	ctx.clearRect(0,0,W,H);
+	if(!fw.length){
+		AF=0;
+		return;
+	}
+	AF = requestAnimationFrame(manageFireworks);
+	
+	for(let i=0;i<fw.length;i++){
+		fw[i].Move();
+		fw[i].Draw();
+		if(fw[i].Done()){
+			fw.splice(i,1);
+			i--;
+		}
+	}
+}
+
+function launchFirework(count=1){
+	for(let i=0;i<count;i++){
+		fw.push(new firework());
+	}
+	if(!AF){
+		requestAnimationFrame(manageFireworks);
+	}
+}
+
+function firework(){
+	this.exploded = 0;
+	this.location = new point(W/2, H);
+	const x = Math.random()*W;
+	const y = (Math.random()*H/2);
+	this.target = new point(x,y);
+	
+	const tempx = (W/2)-x;
+	const tempy = H-y;
+	const hyp = ((tempx**2)+(tempy**2))**.5;
+	
+	const dx = speed*tempx/hyp;
+	const dy = speed*tempy/hyp;
+	this.delta = new point(-dx,-dy);
+	
+	this.hue = Math.floor(Math.random()*360);
+	this.sat = 100;
+	this.alpha = 70;
+	this.fragments = [];
+}
+
+firework.prototype.Done = function(){
+	return this.exploded && this.fragments.length == 0;
+}
+firework.prototype.Draw = function(){
+	
+	if(this.exploded){
+		for(let i=0;i<this.fragments.length;i++){
+			this.fragments[i].Draw();
+		}
+		return;
+	}
+	
+	ctx.beginPath();
+	ctx.strokeStyle = `hsl(${this.hue},${this.sat}%,${this.alpha}%)`;
+	ctx.lineWidth = 2;
+	ctx.moveTo(this.location.x, this.location.y);
+	ctx.lineTo(
+		this.location.x+this.delta.x*2, 
+		this.location.y+this.delta.y*2
+		);
+	ctx.stroke();
+	ctx.closePath();
+}
+firework.prototype.Move = function(){
+	if(this.exploded){
+		for(let i=0;i<this.fragments.length;i++){
+			this.fragments[i].Move();
+			if(this.fragments[i].age < 0){
+				this.fragments.splice(i,1);
+				i--;
+			}
+		}
+		return;
+	}
+	this.location = this.location.plus(this.delta);
+	
+	if(this.location.y < this.target.y){
+		this.Explode();
+	}
+}
+firework.prototype.Explode = function(){
+	//make the particles.
+	this.exploded = 1;
+	const particles = Math.random()*12+20;
+	for(let i=0;i<particles;i++){
+		this.fragments.push(new fragment(this.location.x, this.location.y, this.hue));
+	}
+}
+
+function fragment(x,y,hue){
+	this.location = new point(x,y);
+	const v = Math.random() * twoPI;
+	const px = Math.random() * 5;
+	const py = Math.random() * 5;
+	const vx = Math.cos(v) * px * 2;
+	const vy = Math.sin(v) * py * 2;
+	this.velocity = new point(vx, vy);
+	this.hue = hue+(Math.floor(Math.random()*100)-50);
+	this.sat = 50;
+	this.trail = [];
+	this.age = 100 + Math.random()*50;
+}
+
+fragment.prototype.Draw = function(){
+	const r = 2;
+	for(let i=0;i<this.trail.length;i++){
+		const alpha = 70-(i*5);
+		ctx.beginPath();
+		ctx.fillStyle = `hsl(${this.hue},50%,50%,${alpha}%)`;
+		ctx.arc(this.trail[i].x, this.trail[i].y, r, 0, twoPI);
+		ctx.fill();
+		ctx.closePath();
+	}
+}
+fragment.prototype.Move = function(){
+	this.location = this.location.plus(this.velocity);
+	this.velocity = this.velocity.scale(.95);
+	this.velocity.y -= G;
+	this.trail.unshift(this.location);
+	this.age--;
+	while(this.trail.length > 15){
+		this.trail.pop();
+	}
+}
+
+function initFireworks(){
+	const w = Math.max(document.documentElement.clientWidth) - 10;
+	const h = Math.max(document.documentElement.clientHeight) - 10;
+	
+	F.style.width = w;
+	F.style.height = h;
+	F.width = w;
+	F.height = h;
+	
+	F.style.position = 'absolute';
+	F.style.inset = 0;
+	F.style.border = 'none';
+	
+	H = h;
+	W = w;
+}
+initFireworks();
